a4aea6cd8176ea74f3c4f15cf368e21c2a44fccd
NickSpangler <Spangleyman@mac.com>
2021-02-22 12:57:56 -0500

    first challenge component

---
+++ controlled-form/src/Challenge.js
@@ -0,0 +1,23 @@
+/** * What is wrong with this example, and how would you go  
+* about fixing or improving the component? 
+*/ 
+class App extends React.Component {  
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: this.props.name || 'Anonymous'
+      }
+    }    
+    render() {
+      return (
+        <p>Hello {this.state.name}</p>
+      );  
+    }
+  }
+
+  
+  const App = (props) => {
+      return (
+          <p>Hello {props.name ? props.name : 'Anonymous'}</p>
+      )
+  }
